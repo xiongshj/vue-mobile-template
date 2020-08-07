01bd2eaac84be8cdbec41c4318cad4f45375c872
xiongshj <xiongshjcode@gmail.com>
2020-08-07 14:32:22 +0800

    feat: add svg component

--- vue.config.js
+++ vue.config.js
@@ -25,4 +25,23 @@ module.exports = {
       patterns: [path.resolve(__dirname, 'src/styles/variables.less')],
     },
   },
+
+  chainWebpack(config) {
+    // set svg-sprite-loader
+    config.module
+      .rule('svg')
+      .exclude.add(resolve('src/assets/icons'))
+      .end();
+    config.module
+      .rule('icons')
+      .test(/\.svg$/)
+      .include.add(resolve('src/assets/icons'))
+      .end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]',
+      })
+      .end();
+  },
 };
