52ebb3c8e5bfbdedd8ca1f899009cf212b0ae31c
xiongshj <xiongshjcode@gmail.com>
2020-09-01 16:36:10 +0800

    chore: 修改打包设置

--- vue.config.js
+++ vue.config.js
@@ -6,34 +6,34 @@ function resolve(dir) {
 }
 
 module.exports = {
+  publicPath: './',
+  outputDir: 'dist',
+  assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
+  productionSourceMap: false,
   devServer: {
     overlay: {
       warnings: false,
       errors: true,
     },
   },
-
   configureWebpack: {
     resolve: {
       extensions: ['.js', '.vue', '.json'],
       alias: {
         '@': resolve('src'),
-        // 配置别名，可根据需要进行设置
-        // assets: resolve('src/assets'),
-        // components: resolve('src/components'),
-        // views: resolve('src/views'),
+        assets: resolve('src/assets'),
+        components: resolve('src/components'),
+        views: resolve('src/views'),
       },
     },
   },
-
   pluginOptions: {
     'style-resources-loader': {
       preProcessor: 'less',
       patterns: [path.resolve(__dirname, 'src/styles/variables.less')],
     },
   },
-
   chainWebpack(config) {
     // set svg-sprite-loader
     config.module
